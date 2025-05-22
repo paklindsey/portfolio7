b33c20f6657171836890b01e776af4c1d4971883
Lindsey Pak <lindseypak@mac.mynetworksettings.com>
2025-05-22 07:21:23 -0400

    navbar start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Home />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,23 @@
+import "../styles/navbar.scss";
+import smileIcon from "../images/smileIcon.svg";
+import arrowLeft from "../images/arrowLeft.svg";
+import arrowRight from "../images/arrowRight.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__iconsWrapper">
+        <div className="navbar__iconsWrapper__arrowLeft">
+          <img src={arrowLeft} alt="arrow pointing left" />
+        </div>
+        <div className="navbar__iconsWrapper__smileIcon">
+          <img src={smileIcon} alt="smile face in a circle" />
+        </div>
+        <div className="navbar__iconsWrapper__arrowRight">
+          <img src={arrowRight} alt="arrow poiting right" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
