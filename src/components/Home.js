a739c36ef31958ea98494615290a6c31be082679
Lindsey Pak <lindseypak@mac.mynetworksettings.com>
2025-05-21 21:26:52 -0400

    project init

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import "../styles/home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__text">
+        <div className="home__text__name">
+          <p>Lind</p>
+          <p>sey</p>
+          <p>pak</p>
+        </div>
+        <div className="home__text__titles">
+          <p>brand designer</p>
+          <p>UX/UI designer</p>
+          <p>front end software engineer</p>
+          <p>product manager</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
